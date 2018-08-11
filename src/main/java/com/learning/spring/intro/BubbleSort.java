1d5ae5f2b75c358244690ccedff6657f74e7c364
llenk <ellenkeal@gmail.com>
2018-08-11 16:05:54 -0500

    spring manage dependencies

--- src/main/java/com/learning/spring/intro/BubbleSort.java
+++ src/main/java/com/learning/spring/intro/BubbleSort.java
@@ -1,5 +1,8 @@
 package com.learning.spring.intro;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSort implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		// Logic for Bubble Sort
--- src/main/java/com/learning/spring/intro/IntroApplication.java
+++ src/main/java/com/learning/spring/intro/IntroApplication.java
@@ -2,17 +2,29 @@ package com.learning.spring.intro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class IntroApplication {
+	
+	// what are the beans?
+	// what are the dependencies of the beans?
+	// where to search for beans? no need bc we're in the same package
+	
 
 	public static void main(String[] args) {
 		
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSort());
+		
+		ConfigurableApplicationContext applicationContext = 
+				SpringApplication.run(IntroApplication.class, args);
+		
+		BinarySearchImpl binarySearch = 
+				applicationContext.getBean(BinarySearchImpl.class);
 		
 		int result = binarySearch.binarySearch(new int[] {1, 2, 3}, 2);
 		System.out.println(result);
-		SpringApplication.run(IntroApplication.class, args);
+
 		
 	}
 }
