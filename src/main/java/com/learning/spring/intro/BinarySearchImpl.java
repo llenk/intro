7f1955c0651da7d9ae5261426992a525a64ace64
llenk <ellenkeal@gmail.com>
2018-08-13 14:40:02 -0500

    finished

--- src/main/java/com/learning/spring/intro/BinarySearchImpl.java
+++ src/main/java/com/learning/spring/intro/BinarySearchImpl.java
@@ -8,11 +8,8 @@ public class BinarySearchImpl {
 	
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
-	
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
+
+
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm);
